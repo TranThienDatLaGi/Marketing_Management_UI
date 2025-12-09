6b20b9b756c07e392ac0cdde342a92fa8f7c9557
TranThienDatLaGi <tranthiendatlagi@gmail.com>
2025-12-09 10:36:08 +0700

    update Bills

--- src/config/API.ts
+++ src/config/API.ts
@@ -1,6 +1,6 @@
 // export const API = import.meta.env.VITE_API_URL;
-// export const API = '/api';
-export const API = 'http://127.0.0.1:8000/api';
+export const API = '/api';
+// export const API = 'http://127.0.0.1:8000/api';
 export const LOGIN = `${API}/login`;
 export const LIST_USER = `${API}/getListUser`;
 export const REGISTER = `${API}/register`;
